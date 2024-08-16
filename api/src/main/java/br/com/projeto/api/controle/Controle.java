dd22a424e9fc06633cb203a41b8911b07d4251da
madson cardoso <madson91@gmail.com>
2024-08-16 16:19:32 -0300

    implementando metodos crud no controle

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,18 +1,20 @@
 package br.com.projeto.api.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Cliente;
 import br.com.projeto.api.repositorio.Repositorio;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 @RestController
+@RequestMapping("/cliente")
 public class Controle {
 
     @Autowired
@@ -21,17 +23,28 @@ public class Controle {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello world1";
+        return "Bem Vindo ao Cadastro de Clientes";
     }
 
-    @PostMapping("/cliente")
+    @PostMapping("/")
     public Cliente cadastrar(@RequestBody Cliente c) {
         repo.save(c);
 
         return c;
     }
 
-    @GetMapping("/cliente")
+    @PutMapping("/")
+    public Cliente alterar(@RequestBody Cliente c) {
+        repo.save(c);
+        return c;
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void delete(@PathVariable("codigo") long codigo) {
+        repo.deleteById(codigo);
+    }
+
+    @GetMapping("/all")
     public Iterable<Cliente> buscarAll() {
 
         return repo.findAll();
