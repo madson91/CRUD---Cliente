c486a56da0f83fbac1fbea3042152e1a70d7926a
madson cardoso <madson91@gmail.com>
2024-09-29 10:52:32 -0300

    iniciando contrucao componente front

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +16,11 @@ import br.com.projeto.api.repositorio.Repositorio;
 
 @RestController
 @RequestMapping("/cliente")
+@CrossOrigin(origins = "*")
 public class Controle {
 
     @Autowired
     private Repositorio repo;
-
 
     @GetMapping("/")
     public String hello() {
@@ -49,6 +50,17 @@ public class Controle {
 
         return repo.findAll();
     }
+
+    //tratando erro de usuario inexistente
+    //  @DeleteMapping("/{codigo}")
+    // public ResponseEntity<Void> delete2(@PathVariable("codigo") long codigo) {
+    //     try {
+    //         repo.deleteById(codigo);
+    //         return ResponseEntity.noContent().build();
+    //     } catch (EmptyResultDataAccessException e) {
+    //         return ResponseEntity.notFound().build();
+    //     }
+    // }
 
 
 }
